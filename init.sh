b2364f9a4e4b83c0f553ecd36b0aa04941b60429
majortom327 <vacythe@gmail.com>
2018-01-31 12:30:17 +0100

    Default php project

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir -p ./data ./src
+echo "<!DOCTYPE html>" > ./src/index.php
+echo "<html>" >> ./src/index.php
+echo "<head>" >> ./src/index.php
+echo "<title>TEST</title>" >> ./src/index.php
+echo "</head>" >> ./src/index.php
+echo "<body>" >> ./src/index.php
+echo "<h1>TEST BOILERPLATE</h1>" >> ./src/index.php
+echo "</body>" >> ./src/index.php
+echo "</html>" >> ./src/index.php
